056c5240a75316e17fb6785a9cedc6ae626a565b
MonEmperor <abr.da.king2016@gmail.com>
2021-12-20 01:12:03 -0500

    made it 2d
    planning on going graphical and implementing forces soon as well instead of just kinematics.

--- main.cpp
+++ main.cpp
@@ -19,18 +19,30 @@ using namespace std;
 
 class particle{
     public:
-        float v; //velocity is constant unless changed
-        float a; //acceleration is constant unless changed
+        vector<float> v; //velocity is constant unless changed
+        vector<float> a; //acceleration is constant unless changed
         float m; //mass is constant unless changed
-        float pos = 0.0;
+        vector<float> pos = {0.0, 0.0};
 
-        float move(){
-            pos += v;
+        vector<float> move(){
+            // place new elements at the end of the vector then removes the
+            // first 2 indexes
+            for (int i=0, iter=pos.size(); i<iter; i++){
+                pos.insert(pos.end(), pos[i]+v[i]); 
+            }
+            for (int i=0, iter=pos.size()/2; i<iter; i++){
+                pos.erase(pos.begin());
+            }
             return pos;
         }
 
-        float accelerate(){
-            v +=a;
+        vector<float> accelerate(){
+            for (int i=0, iter=v.size(); i<iter; i++){
+                v.insert(v.end(), v[i]+a[i]); 
+            }
+            for (int i=0, iter=v.size()/2; i<iter; i++){
+                v.erase(v.begin()); 
+            }
             return v;
         }
 
@@ -47,17 +59,20 @@ class particle{
 int main(){
 
     particle p;
-    p.v = 4.0; p.a = -0.5; p.m = 1.0;
-    cout << "created object p(particle)" << endl;
-    cout << "Initial Velocity: " << p.v << endl;
-    cout << "Initial Acceleration: " << p.a <<endl;
+    p.v = {1.0,0}; p.a = {0,-0.5}; p.m = 1.0;
+    
+    cout << "Initial Velocity: [" << p.v[0]<<","<<p.v[1]<<"]" << endl;
+    cout << "Initial Acceleration: [" << p.a[0]<<","<<p.a[1]<<"]" << endl;
+
+    cout << "-----------------------------" <<endl;
     sleep(3);
 
     while (true){
         p.move();
         p.accelerate();
-        cout << "Velocity: " << p.v <<endl;
-        cout << "Position: " << p.pos <<endl;
+        cout<<"Velocity: [" << p.v.front() <<","<< p.v.back() << "]" <<endl;
+        cout<<"Position: [" << p.pos.front() <<","<< p.pos.back() << "]" <<endl;
+        cout<<"-----------------------------" <<endl;
         sleep(1);
     }
     
